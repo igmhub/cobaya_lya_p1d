24daa594252f317841440204fa54993c0fbfb021
jchavesmontero <jchaves@ifae.es>
2026-01-27 14:25:21 +0100

    docstring

--- cobaya_lya_p1d/cobaya_lya_p1d.py
+++ cobaya_lya_p1d/cobaya_lya_p1d.py
@@ -3,125 +3,207 @@ import numpy as np
 
 
 def fit_polynomial(xmin, xmax, x, y, deg=2):
-    """Fit a polynomial on the log of the function, within range"""
+    """
+    Fit a polynomial to the logarithm of a function over a specified range.
+
+    The function performs a polynomial fit to log(y) as a function of log(x)
+    using only data points with xmin < x < xmax. The resulting polynomial
+    can be evaluated to approximate log(y) within the fitting range.
+
+    Parameters
+    ----------
+    xmin, xmax : float
+        Lower and upper bounds of the fitting range in x.
+    x : array_like
+        Independent variable.
+    y : array_like
+        Dependent variable, assumed to be positive.
+    deg : int, optional
+        Degree of the polynomial fit (default: 2).
+
+    Returns
+    -------
+    poly : numpy.poly1d
+        Polynomial representing log(y) as a function of log(x).
+    """
+    # Select data points within the fitting range
     x_fit = (x > xmin) & (x < xmax)
-    # We could make these less correlated by better choice of parameters
+
+    # Fit a polynomial in log–log space; alternative parameterizations
+    # could reduce correlations between polynomial coefficients
     poly = np.polyfit(np.log(x[x_fit]), np.log(y[x_fit]), deg=deg)
+
+    # Return the fitted polynomial as a callable object
     return np.poly1d(poly)
 
 
-def fit_linP_kms(
-    provider,
-    zstar,
-    kp_kms,
-    deg=2,
-    fit_min=0.5,
-    fit_max=2.0,
-):
-    """Given input cosmology, compute linear power at zstar
-    (in km/s) and fit polynomial around kp_kms."""
-
-    k_kms = np.logspace(np.log10(0.5 * kp_kms), np.log10(2.0 * kp_kms), 100)
-
-    ## Get conversion factor from velocity units to comoving
-    H_z = provider.get_Hubble(zstar, units="km/s/Mpc")[0]
-    dvdX = H_z / (1 + zstar)
-    k_Mpc = k_kms * dvdX
-
-    Pk_k, Pk_z, Pk_P = provider.get_Pk_grid(
-        var_pair=("delta_nonu", "delta_nonu"), nonlinear=False
-    )
-
-    P_Mpc = np.exp(np.interp(np.log(k_Mpc), np.log(Pk_k), np.log(Pk_P[0])))
-    P_kms = P_Mpc * dvdX**3
-
-    # specify wavenumber range to fit
-    kmin_kms = fit_min * kp_kms
-    kmax_kms = fit_max * kp_kms
-    # compute ratio
-    P_fit = fit_polynomial(
-        kmin_kms / kp_kms, kmax_kms / kp_kms, k_kms / kp_kms, P_kms, deg=deg
-    )
-
-    Delta2_star = np.exp(P_fit[0]) * kp_kms**3 / (2.0 * np.pi**2)
-    n_star = P_fit[1]
-    # note that the curvature is alpha/2
-    alpha_star = 2.0 * P_fit[2]
-
-    results = {
-        "Delta2_star": Delta2_star,
-        "n_star": n_star,
-        "alpha_star": alpha_star,
-    }
-
-    return results
-
-
 class Cobaya_lya_p1d(Likelihood):
-    params = {
-        "delta2star_mean": 0.379,
-        "delta2star_std": 0.032,
-        "nstar_mean": -2.309,
-        "nstar_std": 0.019,
-        "correlation": -0.1738,
-        "zstar": 3.0,
-        "kstar_kms": 0.009,
-        "speed": 30000,
-    }
+    """
+    Gaussian likelihood for compressed Lyman-alpha forest 1D power-spectrum parameters.
+
+    This likelihood constrains the amplitude and slope of the linear matter power
+    spectrum at a pivot redshift and scale, using compressed parameters
+    (Delta2_star, n_star) derived from the Lyman-alpha forest 1D power spectrum.
+    """
+
+    delta2star_mean: float
+    delta2star_std: float
+    nstar_mean: float
+    nstar_std: float
+    correlation: float
+    zstar: float
+    kstar_kms: float
 
     def initialize(self):
         """
-        Set best-fitting value of the likelihood parameters
+        Initialize the data vector, covariance matrix, and theory options.
+
+        This method constructs the Gaussian data vector and its covariance,
+        and defines the options used to request the linear matter power spectrum
+        from the Boltzmann solver via Cobaya.
         """
 
-        # Initizalize the data vector
-        self.data = np.array(
-            [self.params["delta2star_mean"], self.params["nstar_mean"]]
-        )
+        # Data vector: mean values of the compressed parameters
+        self.data = np.array([self.delta2star_mean, self.nstar_mean])
 
-        # Compute the correlation term
-        cross_term = (
-            self.params["correlation"]
-            * self.params["delta2star_std"]
-            * self.params["nstar_std"]
-        )
+        # Off-diagonal covariance term from the correlation coefficient
+        cross_term = self.correlation * self.delta2star_std * self.nstar_std
 
-        # Initialize the covariance matrix and compute its inverse
+        # Covariance matrix and its inverse
         self.cov = np.array(
             [
-                [self.params["delta2star_std"] ** 2, cross_term],
-                [cross_term, self.params["nstar_std"] ** 2],
+                [self.delta2star_std**2, cross_term],
+                [cross_term, self.nstar_std**2],
             ]
         )
         self.inv_cov = np.linalg.inv(self.cov)
 
-    def get_requirements(self, kmax_Mpc=2.0):
-        """Quantities calculated by a theory code that are needed"""
+        # Options controlling the P(k) grid returned by the theory provider
+        self.options_get_Pk_grid = {
+            "nonlinear": False,
+            "vars_pairs": ("delta_nonu", "delta_nonu"),
+            "kmax_Mpc": 3.0,
+        }
+
+    def get_requirements(self):
+        """
+        Declare theory quantities required by the likelihood.
+
+        Returns
+        -------
+        dict
+            Dictionary specifying the power-spectrum grid and Hubble parameter
+            required from the theory code.
+        """
         reqs = {
             "Pk_grid": {
-                "z": self.params["zstar"],
-                "k_max": kmax_Mpc,
-                "nonlinear": False,
-                "vars_pairs": [("delta_nonu", "delta_nonu")],
+                "z": self.zstar,
+                "k_max": self.options_get_Pk_grid["kmax_Mpc"],
+                "nonlinear": self.options_get_Pk_grid["nonlinear"],
+                "vars_pairs": self.options_get_Pk_grid["vars_pairs"],
             },
-            "Hubble": {"z": [self.params["zstar"]]},
+            "Hubble": {"z": [self.zstar]},
         }
 
         return reqs
 
-    def logp(self, **params_values):
-        """Taking a dictionary of (sampled) nuisance parameter values params_values
-        and return a log-likelihood.
+    def fit_linP_kms(
+        self,
+        deg=2,
+        fit_min=0.5,
+        fit_max=2.0,
+    ):
+        """
+        Compute and locally fit the linear matter power spectrum at zstar.
+
+        The method converts the linear matter power spectrum from comoving
+        units to velocity units (km/s), fits a polynomial in log–log space
+        around kstar_kms, and extracts compressed parameters describing the
+        local amplitude, slope, and curvature.
+
+        Parameters
+        ----------
+        deg : int, optional
+            Degree of the polynomial fit (default: 2).
+        fit_min, fit_max : float, optional
+            Range of the fit in units of k / kstar_kms.
+
+        Returns
+        -------
+        dict
+            Dictionary containing Delta2_star, n_star, and alpha_star.
         """
 
-        # compute value of compressed parameters
-        th_params = fit_linP_kms(
-            self.provider,
-            self.params["zstar"],
-            self.params["kstar_kms"],
+        # k grid in velocity units around the pivot scale
+        k_kms = np.logspace(
+            np.log10(fit_min * self.kstar_kms),
+            np.log10(fit_max * self.kstar_kms),
+            100,
         )
 
-        # Compute the chi^2
+        # Conversion factor from velocity units to comoving units
+        H_z = self.provider.get_Hubble(self.zstar, units="km/s/Mpc")[0]
+        dvdX = H_z / (1 + self.zstar)
+        k_Mpc = k_kms * dvdX
+
+        # Retrieve linear matter power spectrum P(k, z)
+        # The minimum k is set internally by the theory code;
+        # the maximum k is controlled via kmax_Mpc
+        Pk_k, Pk_z, Pk_P = self.provider.get_Pk_grid(
+            var_pair=self.options_get_Pk_grid["vars_pairs"],
+            nonlinear=self.options_get_Pk_grid["nonlinear"],
+        )
+
+        # Ensure requested k range is covered by the theory prediction
+        if k_Mpc[-1] > Pk_k[-1]:
+            raise ValueError(
+                "k_Mpc is not within Pk_k range; increase kmax_Mpc in get_requirements"
+            )
+
+        # Interpolate the power spectrum in log–log space
+        P_Mpc = np.exp(np.interp(np.log(k_Mpc), np.log(Pk_k), np.log(Pk_P[0])))
+
+        # Convert power spectrum to velocity units
+        P_kms = P_Mpc * dvdX**3
+
+        # Polynomial fit of log P(k) around the pivot scale
+        P_fit = fit_polynomial(
+            fit_min, fit_max, k_kms / self.kstar_kms, P_kms, deg=deg
+        )
+
+        # Extract compressed parameters at the pivot scale following
+        # Appendix C of Pedersen+2023 (https://arxiv.org/abs/2209.09895)
+        Delta2_star = (
+            np.exp(P_fit[0]) * self.kstar_kms**3 / (2.0 * np.pi**2)
+        )
+        n_star = P_fit[1]
+        alpha_star = 2.0 * P_fit[2]
+
+        return {
+            "Delta2_star": Delta2_star,
+            "n_star": n_star,
+            "alpha_star": alpha_star,
+        }
+
+    def logp(self, **params_values):
+        """
+        Compute the log-likelihood for a given set of cosmological parameters.
+
+        Parameters
+        ----------
+        **params_values : dict
+            Dictionary of sampled parameter values.
+
+        Returns
+        -------
+        float
+            Log-likelihood value assuming a multivariate Gaussian.
+        """
+
+        # Compute theoretical predictions for the compressed parameters
+        th_params = self.fit_linP_kms()
+
+        # Gaussian chi^2
         theory = np.array([th_params["Delta2_star"], th_params["n_star"]])
         diff = theory - self.data
         chi2 = diff.T.dot(self.inv_cov.dot(diff))
